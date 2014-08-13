4d3f4dbd75ad749b80eb06d55470c5894fe74e65
Alexander Schmitz <arschmitz@gmail.com>
2014-08-13 13:10:42 -0400

    Added support for second signiture of .on and fixed bugs relating to
    strings of multiple events and streamlined code a bit.

--- touchevents-fix.js
+++ touchevents-fix.js
@@ -12,94 +12,69 @@
 		handlers = {},
 		unbound = false;
 
-	function returnFalse() {
-		return false;
-	}
-
-	function unbindTouchEvents() {
-		if (!unbound) {
-			unbound = true;
+	function toggleTouchEventsBindings( bind ) {
+		if ( unbound === bind ) {
+			unbound = !bind;
 			$.each( handlers, function( key, val ) {
 				$.each( val, function( i, fn ) {
-					$document.off( key, fn );
+					$document[ bind ? "on" : "off" ]( key, fn );
 				});
 			});
 		}
 	}
-
-	function rebindTouchEvents() {
-		if (unbound) {
-			unbound = false;
-			$.each( handlers, function( key, val ) {
-				$.each( val, function( i, fn ) {
-					$document.on( key, fn );
-				});
-			});
-		}
+	function handleFocus( e ){
+		console.log( "focus" )
+		toggleTouchEventsBindings( false );
+		$( this ).off( "blur" );
+		console.log( e.data === this )
+		$( this ).on( "blur", function() {
+			console.log( "blur" );
+			toggleTouchEventsBindings( true );
+		});
 	}
 
 	var iosTouchEventsFixer = {
-		protectedData: "protected",
 
 		protectIframes: function() {
 			$( "iframe" ).each( function() {
-				var iframe = this,
-					$iframe = $( iframe ),
-					$contentWindow = $( iframe.contentWindow ),
-					protectedData = iosTouchEventsFixer.protectedData;
+				var $contentWindow = $( this.contentWindow );
 				try {
-					if ( !$iframe.data( protectedData ) ) {
-						$iframe.data( protectedData, true );
-						$contentWindow.bind( "focus", function() {
-							unbindTouchEvents();
-							$contentWindow.one( "blur", function() {
-								rebindTouchEvents();
-								$iframe.data( protectedData, false );
-							} );
-						} );
-					}
-				} catch ( e ) {
-					// noop for iframes that are not of the same domain as the document
-					$.noop();
-				}
+					$contentWindow.off( "focus", null, handleFocus );
+					$contentWindow.on( "focus", null, this.contentWindow, handleFocus );
+				} catch ( e ) {}
 			} );
 		},
 
 		install: function() {
 			$.fn.on = function( types, selector, data, fn ) {
-				if ( data == null && fn == null ) {
-					// ( types, fn )
-					fn = selector;
-					data = selector = undefined;
-				} else if ( fn == null ) {
-					if ( typeof selector === "string" ) {
-						// ( types, selector, fn )
-						fn = data;
-						data = undefined;
-					} else {
-						// ( types, data, fn )
-						fn = data;
-						data = selector;
-						selector = undefined;
+				var touch,
+					func = fn || data || selector,
+					eventObject = types;
+				if ( this.get( 0 ) === document ) {
+					if ( typeof types === "string" ) {
+						eventObject = {};
+						$.each( types.split( " " ), function( i, value ){
+							eventObject[ value ] = func;
+						});
 					}
+					$.each( eventObject, processBinding );
 				}
-				if ( fn === false ) {
-					fn = returnFalse;
-				} else if ( !fn ) {
-					return this;
-				}
-
-				if ( typeof types === "string" ) {
-					if ( /^touch/.test( types ) && ( this.get(0) === document ) ) {
-						if ( !handlers[ types ] ) {
-							handlers[ types ] = [];
+				function processBinding( type, boundFunction ){
+					if ( /^touch/.test( type ) ) {
+						if ( !handlers[ type ] ) {
+							handlers[ type ] = [];
 						}
-						handlers[ types ].push( fn );
-						iosTouchEventsFixer.protectIframes();
+						if( !$.inArray( boundFunction, handlers[ type ] ) ) {
+							handlers[ types ].push( boundFunction );
+						}
+						touch = true;
 					}
+				}
+				if ( touch ) {
+					iosTouchEventsFixer.protectIframes();
 				}
 				return on.call( this, types, selector, data, fn );
-			}
+			};
 		},
 
 		uninstall: function() {
